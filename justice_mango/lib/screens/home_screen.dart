06a378005f73ff34273a00d6fa6b08a18f6cd48a
phuvip <phuvipzz153@gmail.com>
2021-04-01 21:59:09 +0700

    danh iu tất cả các em

--- justice_mango/lib/screens/home_screen.dart
+++ justice_mango/lib/screens/home_screen.dart
@@ -12,20 +12,23 @@ class _HomeScreenState extends State<HomeScreen> {
     return Column(
       children: [
         Image(
-          image: NetworkImage("http://anhnhanh.org/data/images/73/2386/014-fix.jpg?data=net", headers: {
-            "Referer": "http://www.nettruyen.com/",
-          }),
+          image: NetworkImage(
+              "http://anhnhanh.org/data/images/73/2386/014-fix.jpg?data=net",
+              headers: {
+                "Referer": "http://www.nettruyen.com/",
+              }),
         ),
         ElevatedButton(
           onPressed: () async {
             var mangas = await MangaProvider.getLatestManga();
             print(mangas[0].toJson());
-            var chapterInfos = await MangaProvider.getChaptersInfo(mangas[0].id);
+            var chapterInfos =
+                await MangaProvider.getChaptersInfo(mangas[0].id);
             print(chapterInfos[0].toJson());
             var images_url = await MangaProvider.getPages(chapterInfos[0].url);
             print(images_url);
           },
-          child: Text("Mango"),
+          child: Text("tôi muốn Hòa"),
         ),
       ],
     );
