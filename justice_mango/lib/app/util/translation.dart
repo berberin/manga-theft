a45135898aa4c357391b8c8e8b8a3cd8be992010
berberin <52871672+berberin@users.noreply.github.com>
2021-05-06 23:04:31 +0700

    feat[translations]

--- justice_mango/lib/app/util/translation.dart
+++ justice_mango/lib/app/util/translation.dart
@@ -24,6 +24,7 @@ const Map<String, Map<String, String>> translationMap = {
     'readNow': 'Đọc ngay',
     'favorite!': 'Truyện ưa thích!',
     'markFavorite': 'Đánh dấu ưa thích',
+    'alias:': 'Tên khác:',
 
     // reader screen
     'idleLoadingNextChapter': 'Kéo lên để chuyển chương',
@@ -33,12 +34,45 @@ const Map<String, Map<String, String>> translationMap = {
     'idleLoadingPrevChapter': 'Kéo để chuyển về chương trước',
     'failedPrevChapter': 'Không có chương trước.',
     'loadingPrevChapter': 'Đang tải ...',
-    'releasePrevChapter': 'Về chương trước'
+    'releasePrevChapter': 'Về chương trước',
+    'completeText': 'Xong',
   },
   'en_US': {
-    'board': 'Board',
-    'favorite': 'Favorite',
-    'explore': 'Explore',
-    'setting': 'Setting',
+    // home
+    'board': 'Cập nhật',
+    'favorite': 'Ưa thích',
+    'explore': 'Khám phá',
+    'setting': 'Cài đặt',
+
+    // board
+    'loading': 'Đang tải',
+    'canLoading': 'Tải thêm',
+    'idleLoading': 'Kéo lên để tải thêm',
+    'newUpdateFavorite': 'Cập nhật mới cho truyện ưa thích',
+    'noUpdateFound': 'Chưa có cập nhật mới.',
+    'updateJustNow': 'Vừa cập nhật',
+    'reload': 'Tải lại',
+    'hello!': 'Xin chào!',
+
+    // favorite tab
+    'favorites': 'Truyện ưa thích',
+    'dontHaveAnyFavorite': 'Chưa có truyện ưa thích.',
+
+    // manga detail
+    'readNow': 'Đọc ngay',
+    'favorite!': 'Truyện ưa thích!',
+    'markFavorite': 'Đánh dấu ưa thích',
+    'alias:': 'Tên khác:',
+
+    // reader screen
+    'idleLoadingNextChapter': 'Kéo lên để chuyển chương',
+    'canLoadingNextChapter': 'Thả để chuyển chương',
+    'loadingNextChapter': 'Đang tải ...',
+    'failedNextChapter': 'Chưa có chương mới',
+    'idleLoadingPrevChapter': 'Kéo để chuyển về chương trước',
+    'failedPrevChapter': 'Không có chương trước.',
+    'loadingPrevChapter': 'Đang tải ...',
+    'releasePrevChapter': 'Về chương trước',
+    'completeText': 'Xong',
   },
 };
