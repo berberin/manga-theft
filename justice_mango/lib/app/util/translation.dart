9be2366fc7d4a63b6320358cc69377e1ae013fe5
phuvip <phuvipzz153@gmail.com>
2021-05-08 14:00:26 +0700

    fix[translations] update en_US

--- justice_mango/lib/app/util/translation.dart
+++ justice_mango/lib/app/util/translation.dart
@@ -7,7 +7,7 @@ const Map<String, Map<String, String>> translationMap = {
     'setting': 'Cài đặt',
 
     // board
-    'loading': 'Đang tải',
+    'loading': 'Đang tải...',
     'canLoading': 'Tải thêm',
     'idleLoading': 'Kéo lên để tải thêm',
     'newUpdateFavorite': 'Cập nhật mới cho truyện ưa thích',
@@ -39,40 +39,40 @@ const Map<String, Map<String, String>> translationMap = {
   },
   'en_US': {
     // home
-    'board': 'Cập nhật',
-    'favorite': 'Ưa thích',
-    'explore': 'Khám phá',
-    'setting': 'Cài đặt',
+    'board': 'Update',
+    'favorite': 'Favorite',
+    'explore': 'Explore',
+    'setting': 'Setting',
 
     // board
-    'loading': 'Đang tải',
-    'canLoading': 'Tải thêm',
-    'idleLoading': 'Kéo lên để tải thêm',
-    'newUpdateFavorite': 'Cập nhật mới cho truyện ưa thích',
-    'noUpdateFound': 'Chưa có cập nhật mới.',
-    'updateJustNow': 'Vừa cập nhật',
-    'reload': 'Tải lại',
-    'hello!': 'Xin chào!',
+    'loading': 'Loading ...',
+    'canLoading': 'Load more',
+    'idleLoading': 'Pull up to load more',
+    'newUpdateFavorite': 'New updates for favorite mangas',
+    'noUpdateFound': 'No new updates',
+    'updateJustNow': 'Just updated',
+    'reload': 'Reload',
+    'hello!': 'Konichiwa minna-san',
 
     // favorite tab
-    'favorites': 'Truyện ưa thích',
-    'dontHaveAnyFavorite': 'Chưa có truyện ưa thích.',
+    'favorites': 'Favorite mangas',
+    'dontHaveAnyFavorite': 'No favorite mangas',
 
     // manga detail
-    'readNow': 'Đọc ngay',
-    'favorite!': 'Truyện ưa thích!',
-    'markFavorite': 'Đánh dấu ưa thích',
-    'alias:': 'Tên khác:',
+    'readNow': 'Start reading',
+    'favorite!': 'Favorited',
+    'markFavorite': 'Mark as favorite',
+    'alias:': 'Other names:',
 
     // reader screen
-    'idleLoadingNextChapter': 'Kéo lên để chuyển chương',
-    'canLoadingNextChapter': 'Thả để chuyển chương',
-    'loadingNextChapter': 'Đang tải ...',
-    'failedNextChapter': 'Chưa có chương mới',
-    'idleLoadingPrevChapter': 'Kéo để chuyển về chương trước',
-    'failedPrevChapter': 'Không có chương trước.',
-    'loadingPrevChapter': 'Đang tải ...',
-    'releasePrevChapter': 'Về chương trước',
-    'completeText': 'Xong',
+    'idleLoadingNextChapter': 'Pull up to load next chapter',
+    'canLoadingNextChapter': 'Release to move to next chapter',
+    'loadingNextChapter': 'Loading ...',
+    'failedNextChapter': 'There is no new chapter!!',
+    'idleLoadingPrevChapter': 'Pull down to go back to previous chapter',
+    'failedPrevChapter': 'There is no previous chapter!!',
+    'loadingPrevChapter': 'Loading ...',
+    'releasePrevChapter': 'Back to previous chapter',
+    'completeText': 'Done',
   },
 };
