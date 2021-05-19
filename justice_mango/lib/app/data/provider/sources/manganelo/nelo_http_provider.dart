006c3e947485ee9c0d42edc31edb4480aa5c284b
berberin <52871672+berberin@users.noreply.github.com>
2021-05-19 08:54:33 +0700

    fix[sources]: move to nhattruyen

--- justice_mango/lib/app/data/provider/sources/manganelo/nelo_http_provider.dart
+++ justice_mango/lib/app/data/provider/sources/manganelo/nelo_http_provider.dart
@@ -5,7 +5,6 @@ class NeloHttpProvider extends HttpProvider {
   void init() {
     dio.options.headers['Referer'] = 'https://www.manganelo.com/';
     dio.options.headers['Origin'] = 'https://www.manganelo.com';
-    dio.options.headers['Host'] = 'www.manganelo.com';
     dio.options.headers['User-Agent'] =
         'Mozilla / 5.0(Windows NT 10.0; Win64; x64; rv:87.0) Gecko / 20100101 Firefox / 87.0';
   }
--- justice_mango/lib/app/data/provider/sources/nettruyen/nettruyen_http_provider.dart
+++ justice_mango/lib/app/data/provider/sources/nettruyen/nettruyen_http_provider.dart
@@ -3,9 +3,8 @@ import 'package:justice_mango/app/data/provider/http_provider.dart';
 class NettruyenHttpProvider extends HttpProvider {
   @override
   void init() {
-    dio.options.headers['Referer'] = 'http://www.nettruyen.com/';
-    dio.options.headers['Origin'] = 'http://www.nettruyen.com';
-    dio.options.headers['Host'] = 'www.nettruyen.com';
+    dio.options.headers['Referer'] = 'http://www.nhattruyen.com/';
+    dio.options.headers['Origin'] = 'http://www.nhattruyen.com';
     dio.options.headers['User-Agent'] =
         'Mozilla / 5.0(Windows NT 10.0; Win64; x64; rv:87.0) Gecko / 20100101 Firefox / 87.0';
   }
