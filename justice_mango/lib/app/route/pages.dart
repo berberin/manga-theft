8c1236e7cc8083f46bc1b802ba9e9ea6e6121268
berberin <souplua@gmail.com>
2021-06-15 01:07:11 +0700

    fix[binding]: remove reader binding

--- justice_mango/lib/app/route/pages.dart
+++ justice_mango/lib/app/route/pages.dart
@@ -3,8 +3,8 @@ import 'package:justice_mango/app/modules/home/home_binding.dart';
 import 'package:justice_mango/app/modules/home/home_screen.dart';
 import 'package:justice_mango/app/modules/manga_detail/manga_detail_binding.dart';
 import 'package:justice_mango/app/modules/manga_detail/manga_detail_screen.dart';
-import 'package:justice_mango/app/modules/reader/reader_binding.dart';
-import 'package:justice_mango/app/modules/reader/reader_screen.dart';
+// import 'package:justice_mango/app/modules/reader/reader_binding.dart';
+// import 'package:justice_mango/app/modules/reader/reader_screen.dart';
 import 'package:justice_mango/app/route/routes.dart';
 
 class AppPages {
@@ -21,11 +21,12 @@ class AppPages {
       binding: MangaDetailBinding(),
       transition: Transition.cupertino,
     ),
-    GetPage(
-      name: Routes.READER,
-      page: () => ReaderScreen(),
-      binding: ReaderBinding(),
-      transition: Transition.cupertino,
-    ),
+    // note: issue with binding: dispose all controllers
+    // GetPage(
+    //   name: Routes.READER,
+    //   page: () => ReaderScreen(),
+    //   binding: ReaderBinding(),
+    //   transition: Transition.cupertino,
+    // ),
   ];
 }
